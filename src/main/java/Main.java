53d633b5e7418ae624973b7e965e86aadde30c1a
egorovvladlen <egorov.vladlen@gmail.com>
2021-05-17 23:40:15 +0300

    Updated: data adding, deleting and changing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,11 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -7,33 +15,54 @@ public class Main {
 
 
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/skillbox";
-        String user = "root";
-        String pass = "AngelinaJolie1";
 
+        // sessionFactory должна быть только одна, можно создать для ее создания отдельный статический класс (синглтон)
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
 
-        try {
-            Connection connection = DriverManager.getConnection(url, user, pass);
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
 
-            Statement statement = connection.createStatement();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
 
-            ResultSet resultSet = statement.executeQuery("SELECT name, count(MONTH(subscription_date))/(TIMESTAMPDIFF(MONTH,min(subscription_date),max(subscription_date))) as average\n" +
-                    "FROM courses c JOIN subscriptions s ON c.id = s.course_id \n" +
-                    "GROUP BY name;");
+        // code case 1 print name and students count
 
-            while(resultSet.next()) {
-                String courseName = resultSet.getString("name");
-                String count = resultSet.getString("average");
-                System.out.println(courseName + " " + count);
-            }
+        Session session = sessionFactory.openSession();
+/*
+        Course course = session.get(Course.class, 1);
 
-            resultSet.close();
-            statement.close();
-            connection.close();
+        System.out.println("\"" + course.getName() + "\"" + " " + course.getStudentCount());
+*/
+        // code case 2 data adding
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Transaction transaction1 = session.beginTransaction();
+
+        Course course1 = new Course();
+        course1.setName("Новый курс");
+        course1.setType(CourseType.BUSINESS);
+        course1.setTeacherId(1);
+
+        session.save(course1);
+        System.out.println(course1.getId());
+        transaction1.commit();
+
+        // code case 3 data replacement
+
+        Transaction transaction2 = session.beginTransaction();
+
+        Course course2 = session.get(Course.class, 45);
+        course2.setName("Измененный курс");
+
+        transaction2.commit();
+
+        // code case 4 data delete
+
+        Transaction transaction3 = session.beginTransaction();
+
+        Course course3 = session.get(Course.class, 56);
+        session.delete(course3);
+
+        transaction3.commit();
+
+        sessionFactory.close();
 
     }
 }
